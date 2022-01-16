d3a7fad472f1d7323377b6af9be02f0e207ec0c9
Vilius Kiškis <vilius.kiskis@gmail.com>
2022-01-16 20:57:07 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+/* console.log('Pasileido')
+
+for (var i = 0; i < 10; i++) {
+    console.log('Pasileido ' + i)
+} */
+
+const component = <h1>Hello, world</h1>;
+ReactDOM.render(
+component,
+document.getElementById('root')
+);
