40d584727c1a1070509d0da742aa888f043d1b4e
Vilius Kiškis <vilius.kiskis@gmail.com>
2022-01-17 15:12:37 +0200

    react example with vanillaJS

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ for (var i = 0; i < 10; i++) {
     console.log('Pasileido ' + i)
 } */
 
-const component = <h1>Hello, world</h1>;
-ReactDOM.render(
-component,
-document.getElementById('root')
+React.createClass = createReactClass;
+var HelloComponent = React.createClass({
+    render: function () {
+        return React.createElement(
+            "div",
+            null,
+            "Hello ",
+            this.props.name
+        );
+    }
+});
+
+ReactDOM.render(React.createElement(
+    HelloComponent,
+    { name: "Jane" }),
+    document.getElementById('root')
 );
