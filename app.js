d5dd72d1e7dbb971f406b333d4bbed40309a9321
Vilius Kiškis <vilius.kiskis@gmail.com>
2022-01-18 18:30:19 +0200

    removed unnecessary code from app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/* console.log('Pasileido')
-
-for (var i = 0; i < 10; i++) {
-    console.log('Pasileido ' + i)
-} */
-
 class HelloComponent extends React.Component {
     render() {
         return <div>Hello {this.props.name}</div>;
