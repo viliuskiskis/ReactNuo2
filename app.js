4ef4ec91f428c9c607099e59235ba8a797455827
Vilius Kiškis <vilius.kiskis@gmail.com>
2022-01-18 19:39:35 +0200

    2 uzd 4a komponentas su atributu

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+// vėliau dar ir import PropTypes from 'prop-types';
 class HelloComponent extends React.Component {
     render() {
-        return <div>Hello {this.props.name}</div>;
+        return (<div>Hello {this.props.name}</div>);
     }
 }
+HelloComponent.propTypes = {
+    name: PropTypes.string.isRequired
+}
 ReactDOM.render(
-    <HelloComponent name="Jane" />,
+    (<HelloComponent name="Jane" />),
     document.getElementById('root')
 );
